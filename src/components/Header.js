01b91a8a9a917d5f0713b52d776027b405d01f33
RachelGcp <rysy3692@gmail.com>
2025-11-02 16:50:22 +0200

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,8 +15,7 @@ const tamar2="good luck!!!!!"
       <div className="container mx-auto flex justify-between items-center px-4">
        
         <h1 className="text-2xl font-bold">Shopping App</h1>
-        <h2 className="text-2xl font-bold">Hello to our Shopping</h2>
-        <h1 className="text-2xl font-bold">Naama & Tovi</h1>
+        <h2 className="text-2xl font-bold">Hello to our Shopping</h2> 
         <h1 className="text-2xl font-bold">Shopping App</h1>
         <h1 className >hello </h1>
         <h1>{tamarrotan}</h1>
